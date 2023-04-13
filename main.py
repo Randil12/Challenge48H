403e69d53197caedf3c047ac9a5ae16d1f6c6bc8
Randil12 <nguyen_david2003@outlook.fr>
2023-04-13 16:01:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-import discord
+import discord 
 from discord.ext import commands
+from discord import app_commands
+import typing
 
-intents = discord.Intents().all()
-bot = commands.Bot(command_prefix="!", intents=intents, case_insensitive=True)
-bot.remove_command('help')
 
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title="Help command", description="!ping test", color=discord.Color.blue())
-    await ctx.channel.send(embed=embed)
+class abot(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.all())
+        self.synced = False
 
-@bot.command()
-async def ping(ctx):
-    await ctx.reply("pong")    
+    async def on_ready(self):
+        await tree.sync()
+        self.synced = True
+        print("Bot is online")
 
-bot.run("OTAxMzkzNzExNDQxNzcyNjA0.G5YQRm.TmuyTs47KDTEWHOdZ7qPVOYhTrz4e0iEP6XyvM")
+bot = abot()
+tree = app_commands.CommandTree(bot)
+
+
+@tree.command(name="Locate", description="get basic rank infos")
+async def self(interation: discord.Interaction,name:str , region: typing.Literal["EUW","EUNE","NA","BR","JP","KR","LA","LAS","OC","TR","RU"], queue: typing.Literal["SoloQ","FelxQ"]):
+    await interation.response.send_message("str à la con")
+
+
+
+bot.run('OTAxMzkzNzExNDQxNzcyNjA0.G5YQRm.TmuyTs47KDTEWHOdZ7qPVOYhTrz4e0iEP6XyvM')    
