e3bb76df383c02f01b482353272f126f387efb3d
lukas <lukas.cristofaro@ynov.com>
2023-04-14 11:38:55 +0200

    add pin on the map

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import folium
 from selenium.webdriver.chrome.options import Options
 import plotly.express as px
 import plotly.io as pio
+from PIL import Image, ImageDraw
+
 
 
 class abot(discord.Client):
@@ -29,10 +31,10 @@ def create_map(lat, lon):
     m = folium.Map(location=[lat, lon], zoom_start=13)
     return m
 
-@tree.command(name="locate", description="get basic rank infos")
+@tree.command(name="locate", description="localise une adresse")
 async def self(interation: discord.Interaction,ville:str , adresse:str):
     geolocator = Nominatim(user_agent="my_application")
-    location = geolocator.geocode(f'{ville} {adresse}')
+    location = geolocator.geocode(f"{ville} {adresse}")
 
     latitude, longitude = location.latitude, location.longitude
 
@@ -44,14 +46,32 @@ async def self(interation: discord.Interaction,ville:str , adresse:str):
         margin=dict(l=0, r=0, t=0, b=0)
     )
     
-
     # Enregistrer l'image au format PNG
     pio.write_image(fig, 'carte_lyon.png')
+    image = Image.open('carte_lyon.png')
+
+    # Obtenir les dimensions de l'image
+    width, height = image.size
+
+    # Créer un objet ImageDraw pour dessiner sur l'image
+    draw = ImageDraw.Draw(image)
+
+    # Dessiner un cercle rouge au centre de l'image
+    center_x = width // 2
+    center_y = height // 2
+    radius = 5  # ajuster la taille du cercle si nécessaire
+    draw.ellipse((center_x - radius, center_y - radius, center_x + radius, center_y + radius), fill='red')
+    image.save('carte_lyon_modifiee.png')
+
 
     # Charger le fichier d'image
-    file = discord.File("carte_lyon.png", filename="carte_lyon.png")
+    file = discord.File("carte_lyon_modifiee.png", filename="carte_lyon.png")
 
     await interation.response.send_message(file=file)
 
+
+@tree.command(name="test", description="test")
+async def self(interation: discord.Interaction,name:str):
+    await interation.response.send_message(f'Oui + {name}')
 
 bot.run('OTAxMzkzNzExNDQxNzcyNjA0.G5YQRm.TmuyTs47KDTEWHOdZ7qPVOYhTrz4e0iEP6XyvM')
