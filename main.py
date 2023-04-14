34fbaf08522db9adf1b43c545fc4c0a7d1ebd626
lukas <lukas.cristofaro@ynov.com>
2023-04-14 09:04:03 +0200

    add create map

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import discord 
+import plotly.express as px
+import plotly.io as pio
+import discord
+from discord.ext import commands
+import typing
 from discord.ext import commands
 from discord import app_commands
-import typing
-
+from discord import File
 
 class abot(discord.Client):
     def __init__(self):
@@ -17,11 +20,23 @@ class abot(discord.Client):
 bot = abot()
 tree = app_commands.CommandTree(bot)
 
+@tree.command(name="locate", description="jsp")
+async def send_image(interation,  lat: str, lon: str, zoom: int):
+    
+    fig = px.scatter_mapbox(lat=[lat], lon=[lon], zoom=zoom)  # zoom max 18 min 0
+    fig.update_layout(mapbox_style='open-street-map')
 
-@tree.command(name="Locate", description="get basic rank infos")
-async def self(interation: discord.Interaction,name:str , region: typing.Literal["EUW","EUNE","NA","BR","JP","KR","LA","LAS","OC","TR","RU"], queue: typing.Literal["SoloQ","FelxQ"]):
-    await interation.response.send_message("str à la con")
+    fig.update_layout(
+        margin=dict(l=0, r=0, t=0, b=0)
+    )
 
+    pio.write_image(fig, 'carte_lyon.png')
 
+    file = discord.File("carte_lyon.png", filename="carte_lyon.png")
+    await interation.send(file=file)
+
+@tree.command(name="activity", description="jsp")
+async def self1(interation: discord.Interaction,activité:str , region: typing.Literal["EUW","EUNE","NA","BR","JP","KR","LA","LAS","OC","TR","RU"], queue: typing.Literal["SoloQ","FelxQ"]):
+    await interation.response.send_message("str à la con activity")
 
 bot.run('OTAxMzkzNzExNDQxNzcyNjA0.G5YQRm.TmuyTs47KDTEWHOdZ7qPVOYhTrz4e0iEP6XyvM')    
---
+++ maptest.py
@@ -0,0 +1,35 @@
+import plotly.express as px
+import plotly.io as pio
+import discord
+from discord.ext import commands
+
+
+# Créer un bot Discord
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+# Définir une commande pour envoyer l'image
+@bot.event
+async def on_ready():
+    print('Bot is ready')
+
+
+@bot.command()
+async def send_image(ctx,  lat: str, lon: str, zoom: int):
+
+
+    fig = px.scatter_mapbox(lat=[lat], lon=[lon], zoom=zoom)  # zoom max 18 min 0
+    fig.update_layout(mapbox_style='open-street-map')
+
+    fig.update_layout(
+        margin=dict(l=0, r=0, t=0, b=0)
+    )
+
+    # Enregistrer l'image au format PNG
+    pio.write_image(fig, 'carte_lyon.png')
+
+    # Charger le fichier d'image
+    file = discord.File("carte_lyon.png", filename="carte_lyon.png")
+
+    await ctx.send(file=file)
+
+bot.run('OTAxMzkzNzExNDQxNzcyNjA0.G5YQRm.TmuyTs47KDTEWHOdZ7qPVOYhTrz4e0iEP6XyvM')    
